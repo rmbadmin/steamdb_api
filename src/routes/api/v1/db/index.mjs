d8b1d83fcd49d796a517270634e1fd1a1d00289d
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2020-10-05 14:52:01 -0400

    Add memory size function

--- src/routes/api/v1/db/index.mjs
+++ src/routes/api/v1/db/index.mjs
@@ -13,6 +13,9 @@ export default async (fastify, options, next) => {
     var size = (await fs.stat(path.resolve('./dump.rdb'))).size
     var size_formatted = formatBytes(size)
     var size_age = new Date().getTime()
+    var size_mem = Number((await fastify.redis.info('memory')).split('\n')[1].split(':')[1])
+    var size_mem_formatted = formatBytes(size_mem)
+    var size_mem_age = new Date().getTime()
     fastify.route({
         method: 'GET',
         url: '/size',
@@ -37,6 +40,33 @@ export default async (fastify, options, next) => {
                 size_age = new Date().getTime()
             } else {
                 res.type('application/json').send(`{"size":${size},"formatted":"${size_formatted}"}`)
+            }
+        }
+    })
+    fastify.route({
+        method: 'GET',
+        url: '/size/mem',
+        schema: {
+            tags: ['DB'],
+            response: {
+                200: {
+                    size: {
+                        type: 'number'
+                    },
+                    formatted: {
+                        type: 'string'
+                    }
+                }
+            }
+        },
+        handler: async (req, res) => {
+            if ((new Date().getTime() - size_mem_age) > 4 * 60 * 1000) {
+                res.type('application/json').send(`{"size":${size},"formatted":"${size_mem_formatted}"}`)
+                size_mem = Number((await fastify.redis.info('memory')).split('\n')[1].split(':')[1])
+                size_mem_formatted = formatBytes(size_mem)
+                size_mem_age = new Date().getTime()
+            } else {
+                res.type('application/json').send(`{"size":${size_mem},"formatted":"${size_mem_formatted}"}`)
             }
         }
     })
