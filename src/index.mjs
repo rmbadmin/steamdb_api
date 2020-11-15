02ef883911478c904b78ca281c8b2bac3c44c984
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2020-11-15 00:07:17 -0500

    Remove unused function

--- src/index.mjs
+++ src/index.mjs
@@ -75,12 +75,3 @@ fastify.addHook('onReady', (done) => {
 fastify.register(import('./routes/index.mjs'))
 fastify.listen(process.env.PORT || 3000, '0.0.0.0').then(e => console.log(`Listening On ${e}`))
 if (!_fs.existsSync(path.resolve('./avatars'))) await fs.mkdir(path.resolve('./avatars'))
-
-function formatBytes(a, b = 2) {
-    if (0 === a) return "0 Bytes";
-    const c = 0 > b ? 0 : b,
-        d = Math.floor(Math.log(a) / Math.log(1024));
-    return parseFloat((a / Math.pow(1024, d)).toFixed(c)) + " " + ["Bytes", "KB", "MB", "GB",
-        "TB", "PB", "EB", "ZB", "YB"
-    ][d]
-}
