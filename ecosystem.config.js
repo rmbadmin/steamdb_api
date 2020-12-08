4ec5d1ca5ab8eeb033acd1cdf85782e030bde5d9
Turbo Biscuit <55338035+TurboBiscuit@users.noreply.github.com>
2020-12-07 19:00:40 -0500

    Update deploy options

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,19 +3,19 @@ module.exports = {
         {
             name:'SteamDB-API',
             script:'src/index.mjs',
-            instances  : 'max',
+            instances  : 1,
             exec_mode  : "cluster",
             env:{
-                PORT:3010,
+                PORT:3004,
                 REDIS_HOST:'localhost',
-                REDIS_PORT:4009
+                REDIS_PORT:6379
             }
         }
     ],
     deploy:{
         prod:{
             user:'root',
-            host:['hermes.trbo.sh'],
+            host:['caerus'],
             ref:'origin/prod',
             repo:'git@github.com:SteamDB-API/api',
             path:'/root/node/steamdb/api',
